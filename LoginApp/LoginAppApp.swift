00bafeb2bac20406d6dc80660af754b292a60a65
Marat <Shagiakhmetov2499@gmail.com>
2021-06-09 14:03:40 +0300

    Initial Commit

---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by Marat Shagiakhmetov on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
